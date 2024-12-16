85e144eb873893a29cdeb7267ee3432505cdf288
TeePlunder <leon.bergmann23@gmail.com>
2024-12-16 12:50:06 +0100

    feat: add commands

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
 // The purpose of this function will be to split the users input into "words" based on whitespace.
 // It should also lowercase the input and trim any leading or trailing whitespace
 func cleanInput(text string) []string {
@@ -14,8 +20,22 @@ func cleanInput(text string) []string {
 	return strings.Fields(text)
 }
 
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+
+	commands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 
 	for {
 		fmt.Print("Pokedex > ")
@@ -28,9 +48,17 @@ func main() {
 			fmt.Fprintln(os.Stderr, "shouldn't see an error scanning a string")
 		}
 
-		cmd := scanner.Text()
-		words := cleanInput(cmd)
-		first := strings.ToLower(words[0])
-		fmt.Printf("Your command was: %s\n", first)
+		input := scanner.Text()
+		words := cleanInput(input)
+		firstWord := strings.ToLower(words[0])
+
+		if cmd, exists := commands[firstWord]; exists {
+			callbackErr := cmd.callback()
+			if callbackErr != nil {
+				fmt.Println("Error:", callbackErr)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
 	}
 }
