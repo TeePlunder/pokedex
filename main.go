91365aadbb48b76069a079ac15a7c84fd26e5610
TeePlunder <leon.bergmann23@gmail.com>
2024-12-16 14:16:11 +0100

    feat: adjust main to use the client

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"github.com/teeplunder/pokedexcli/internal/api"
 	"github.com/teeplunder/pokedexcli/internal/cli"
 )
 
 func main() {
-	app := cli.NewCLI()
+	client := api.NewClient("https://pokeapi.co/api/v2/")
+	app := cli.NewCLI(client)
 	app.Run()
 }
