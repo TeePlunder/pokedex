2e50df55157ba0df490efb632682b164f2cfccd2
TeePlunder <leon.bergmann23@gmail.com>
2024-12-16 13:09:22 +0100

    feat: change structure of code to save commands inside a pointer to share states and make it more readable

--- main.go
+++ main.go
@@ -10,7 +10,60 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*CLI) error
+}
+
+type CLI struct {
+	commands map[string]cliCommand
+}
+
+func NewCLI() *CLI {
+	return &CLI{
+		commands: map[string]cliCommand{
+			"exit": {
+				name:        "exit",
+				description: "Exit the Pokedex",
+				callback:    commandExit,
+			},
+			"help": {
+				name:        "help",
+				description: "Displays a help message",
+				callback:    commandHelp,
+			},
+		},
+	}
+}
+
+func (cli *CLI) Run() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			fmt.Fprintln(os.Stderr, "Error reading input")
+			break
+		}
+
+		input := scanner.Text()
+		words := cleanInput(input)
+
+		if len(words) <= 0 {
+			fmt.Println("Please enter a command. Type 'help' to see all available commands")
+			continue
+		}
+
+		firstWord := strings.ToLower(words[0])
+
+		if cmd, exists := cli.commands[firstWord]; exists {
+			// Execute the command callback
+			if err := cmd.callback(cli); err != nil {
+				fmt.Println("Error:", err)
+			}
+		} else {
+			cli.unknownCommand()
+		}
+	}
 }
 
 // The purpose of this function will be to split the users input into "words" based on whitespace.
@@ -20,45 +73,27 @@ func cleanInput(text string) []string {
 	return strings.Fields(text)
 }
 
-func commandExit() error {
+func commandExit(cli *CLI) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
+func commandHelp(cli *CLI) error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Printf("Usage:\n\n")
+	for _, cmd := range cli.commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	return nil
+}
+
+func (cli *CLI) unknownCommand() error {
+	fmt.Println("Unkown Command")
+	return nil
+}
+
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	commands := map[string]cliCommand{
-		"exit": {
-			name:        "exit",
-			description: "Exit the Pokedex",
-			callback:    commandExit,
-		},
-	}
-
-	for {
-		fmt.Print("Pokedex > ")
-
-		if !scanner.Scan() {
-			fmt.Fprintln(os.Stderr, "shouldn't see an error scanning a string")
-		}
-
-		if err := scanner.Err(); err != nil {
-			fmt.Fprintln(os.Stderr, "shouldn't see an error scanning a string")
-		}
-
-		input := scanner.Text()
-		words := cleanInput(input)
-		firstWord := strings.ToLower(words[0])
-
-		if cmd, exists := commands[firstWord]; exists {
-			callbackErr := cmd.callback()
-			if callbackErr != nil {
-				fmt.Println("Error:", callbackErr)
-			}
-		} else {
-			fmt.Println("Unknown command")
-		}
-	}
+	cli := NewCLI()
+	cli.Run()
 }
