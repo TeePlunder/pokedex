7a9a9b3cb8f89cff0d36bfa0eda7979de7c67411
TeePlunder <leon.bergmann23@gmail.com>
2024-12-18 11:48:00 +0100

    feat: use net/url module to merge the urls together

--- internal/api/client.go
+++ internal/api/client.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/url"
 )
 
 type Client struct {
@@ -17,10 +18,21 @@ func NewClient(baseUrl string) *Client {
 }
 
 // fetches the resource at the given path and unmarshals the JSON response into v.
-func (c *Client) getResource(path string, v interface{}) error {
-	url := fmt.Sprintf("%s/%s", c.BaseUrl, path)
+func (c *Client) getResource(urlPath string, v interface{}) error {
+	base, err := url.Parse(c.BaseUrl)
+	if err != nil {
+		return fmt.Errorf("failed to parse base URL: %w", err)
+	}
 
-	resp, err := http.Get(url)
+	paramUrl, err := url.Parse(urlPath)
+	if err != nil {
+		return fmt.Errorf("failed to parse relative URL: %w", err)
+	}
+
+	// merges base url and param url correctly together (with query params)
+	reqUrl := base.ResolveReference(paramUrl)
+
+	resp, err := http.Get(reqUrl.String())
 
 	if err != nil {
 		return fmt.Errorf("failed to perform GET request\n %w", err)
