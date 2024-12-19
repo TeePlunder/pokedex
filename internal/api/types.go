b79dacbc613dc988ffddade62b4e23f2fcb3f928
TeePlunder <leon.bergmann23@gmail.com>
2024-12-19 18:48:23 +0100

    feat: adjust pokemon response

--- internal/api/types.go
+++ internal/api/types.go
@@ -41,5 +41,23 @@ type LocationAreaDetailsResponse struct {
 }
 
 type PokemonResponse struct {
-	BaseExperience int `json:"base_experience"`
+	BaseExperience int    `json:"base_experience"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+	Name           string `json:"name"`
+	Stats          []struct {
+		BaseStat int `json:"base_stat"`
+		Effort   int `json:"effort"`
+		Stat     struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Slot int `json:"slot"`
+		Type struct {
+			Name string `json:"name"`
+			URL  string `json:"url"`
+		} `json:"type"`
+	} `json:"types"`
 }
